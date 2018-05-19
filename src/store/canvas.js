d722ecb8cf13d30f0e8276b6e50c4d7d8226fb5b
OrcaXS Suen <sun16281394@gmail.com>
2018-05-20 00:27:22 +0800

    Add cursorPos to vuex

--- src/store/canvas.js
+++ src/store/canvas.js
@@ -1,17 +1,30 @@
 const canvas = {
   state: () => ({
     zoomLevel: 1,
+    currentCursorPosition: {
+      x: 0,
+      y: 0,
+    },
   }),
 
   mutations: {
     SET_ZOOM(state, { zoomLevel }) {
       state.zoomLevel = zoomLevel;
     },
+
+    SET_CURSOR_POS(state, { cursorCoordinates }) {
+      state.currentCursorPosition.x = cursorCoordinates.x;
+      state.currentCursorPosition.y = cursorCoordinates.y;
+    },
   },
 
   actions: {
     setZoomLevel({ commit }, { zoomLevel }) {
       commit('SET_ZOOM', { zoomLevel });
+    },
+
+    setCursorPosition({ commit }, { cursorCoordinates }) {
+      commit('SET_CURSOR_POS', { cursorCoordinates });
     },
   },
 };
