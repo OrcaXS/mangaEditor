4c8c9bd487164b8be9f5ef893682827837e0a868
OrcaXS Suen <sun16281394@gmail.com>
2018-05-22 16:48:47 +0800

    Remove TCanvas.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Upload from '@/components/Upload';
 import Canvas from '@/components/Canvas';
-import TCanvas from '@/components/TCanvas';
 
 Vue.use(Router);
 
@@ -18,11 +17,6 @@ export default new Router({
       path: '/canvas/:file_id',
       name: 'canvas',
       component: Canvas,
-    },
-    {
-      path: '/tcanvas/',
-      name: 'tcanvas',
-      component: TCanvas,
     },
   ],
 });
