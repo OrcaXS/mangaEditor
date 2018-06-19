1338590b3cfaa372b5470f19044950974f85d63f
OrcaXS Suen <sun16281394@gmail.com>
2018-06-20 01:09:17 +0800

    Add font selectors to RightPanel

--- src/components/Editor/RightPanel.vue
+++ src/components/Editor/RightPanel.vue
@@ -1,7 +1,61 @@
 <template>
   <div class="RightPanel">
-    <div class="RightPanel-balloons">
-      <div class="RightPanel-category">Styles</div>
+    <div class="RightPanel-styles">
+      <div class="RightPanel-category">Text Content</div>
+      <div class="RightPanel-content">
+        <p v-if="selectedTextAreaIdx">{{ selectedTextArea.textContent }}</p>
+        <p v-else>(empty)</p>
+      </div>
+      <div class="RightPanel-category">Font &amp; Styles</div>
+      <div class="RightPanel-fonts">
+        <div class="RightPanel-select">
+          <select
+            v-model="selectedFontFamily"
+            name="fontFamily"
+            @change="setTextAreaStyle"
+          >
+            <option value="arial">Arial</option>
+            <option value="comicsans">Comic Sans</option>
+          </select>
+        </div>
+        <div class="RightPanel-select">
+          <select
+            v-model="selectedFontStyle"
+            name="fontStyle"
+            @change="setTextAreaStyle"
+          >
+            <option value="normal">normal</option>
+            <option value="italic">italic</option>
+            <option value="oblique">oblique</option>
+          </select>
+        </div>
+        <div class="RightPanel-select">
+          <select
+            v-model="selectedFontSize"
+            name="fontSize"
+            @change="setTextAreaStyle"
+          >
+            <option
+              v-for="size in fontSizes"
+              :key="size"
+              :value="size"
+            >{{ size }}</option>
+          </select>
+        </div>
+        <div class="RightPanel-select">
+          <select
+            v-model="selectedFontWeight"
+            name="fontWeight"
+            @change="setTextAreaStyle"
+          >
+            <option
+              v-for="size in fontWeights"
+              :key="size"
+              :value="size"
+            >{{ size }}</option>
+          </select>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +67,35 @@ export default {
   name: 'EditorRightPanel',
   components: {
     FontAwesomeIcon,
+  },
+
+  data() {
+    return {
+      selectedFontSize: '10',
+      selectedFontFamily: 'arial',
+      selectedFontStyle: 'normal',
+      selectedFontWeight: '400',
+      fontSizes: ['9', '10', '11', '12', '13', '14', '16', '18', '24', '30', '36', '48', '64', '72', '96', '144', '288'],
+      fontWeights: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    };
+  },
+
+  computed: {
+    selectedTextAreaIdx() {
+      return this.$store.state.canvas.currentTextArea;
+    },
+
+    selectedTextArea() {
+      return this.$store.state.canvas.file[this.$route.params.file_id].textAreas[this.selectedTextAreaIdx];
+    },
+  },
+
+  methods: {
+    setTextAreaStyle() {
+      this.$store.dispatch('setTextAreaStyle', {
+        id: this.$route.params.file_id, idx: this.selectedTextAreaIdx, fontFamily: this.selectedFontFamily, fontStyle: this.selectedFontStyle, fontSize: this.selectedFontSize, fontWeight: this.selectedFontWeight
+      });
+    },
   },
 };
 </script>
@@ -37,9 +120,16 @@ export default {
   color: config('colors.grey-darker');
 }
 
-.RightPanel-balloons {
+.RightPanel-category {
+  background-color: config('colors.peach-light-1');
+  text-align: center;
+  padding: .25rem 0;
+  /* margin: .1rem 0; */
 }
 
-.RightPanel-bg {
+.RightPanel-content {
+  font-weight: 500;
+  padding: .5rem .1rem;
+  color: config('colors.grey-darker');
 }
 </style>
