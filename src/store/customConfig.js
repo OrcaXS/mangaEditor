b094dd179c747fd18a560afdcd57ecc8bcd81f52
OrcaXS Suen <sun16281394@gmail.com>
2018-07-12 16:50:55 +0800

    Finish config page

--- src/store/customConfig.js
+++ src/store/customConfig.js
@@ -9,11 +9,17 @@ const customConfig = {
     ADD_FONT_FAMILY(state, { name }) {
       state.customFonts.push(name);
     },
+    REMOVE_FONT_FAMILY(state, { idx }) {
+      state.customFonts.splice(idx, 1);
+    },
   },
 
   actions: {
     addFontFamily({ commit }, { name }) {
       commit('ADD_FONT_FAMILY', { name });
+    },
+    removeFontFamily({ commit }, { idx }) {
+      commit('REMOVE_FONT_FAMILY', { idx });
     },
   },
 };
--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,35 +1,60 @@
 <template>
   <layout-default>
-    <div class="Config-Page">
+    <div class="Config-page">
       <h2
         v-t="{ path: 'config.font_management' }"
-        class="Config-Heading"
+        class="Config-heading"
       />
-      <div class="Config-InputArea">
-        <input
-          :placeholder="fontInputPlaceHolder"
-          v-model="fontInput"
-          class="Config-FontInput"
+      <p v-t="{ path: 'config.font_explanation' }"/>
+      <div class="Config-mainArea">
+        <form
+          class="Config-inputArea"
+          @submit="checkForm"
         >
-        <button
-          v-t="{ path: 'config.add_font' }"
-          class="Config-AddFontBtn"
-          @click="addFontFamily"
-        />
+          <input
+            id="fontname"
+            :placeholder="fontInputPlaceHolder"
+            v-model="fontInput"
+            class="Config-fontInput"
+            type="text"
+            name="fontname"
+            required
+          >
+          <input
+            :value="$t('config.add_font')"
+            type="submit"
+            class="Config-addFontBtn"
+          >
+        </form>
+        <div
+          :style="selectedFontStyle"
+          class="Config-testArea"
+        >
+          <p lang="zh-cn">朝辞白帝彩云间</p>
+          <p lang="zh-tw">朝辭白帝彩雲間</p>
+          <p lang="ja-JP">朝に辭す白帝彩雲の間</p>
+          <p lang="en-US">The quick brown fox jumps over the lazy dog</p>
+        </div>
       </div>
-      <div class="Config-FontList">
+      <div class="Config-fontList">
         <div
           v-for="(font, idx) in fontList"
           :key="idx"
-          class="Config-FontItem"
+          class="Config-fontItem"
         >
-          <FontAwesomeIcon :icon="['fas', 'times']" />
-          <span
+          <button>
+            <FontAwesomeIcon
+              :icon="['fas', 'times']"
+              @click="removeFontFamily({ idx })"
+            />
+          </button>
+          <button
             :style="{ fontFamily: `${font}, sans-serif` }"
-            class="Config-FontName"
+            :class="[ idx === selectedFontFamilyIdx ? 'Config-fontName--selected' : 'Config-fontName']"
+            @click="selectFont({ idx })"
           >
             {{ font }}
-          </span>
+          </button>
         </div>
       </div>
     </div>
@@ -46,7 +71,8 @@ export default {
   },
   data() {
     return {
-      fontInput: '',
+      fontInput: null,
+      selectedFontFamilyIdx: null,
     };
   },
 
@@ -57,27 +83,52 @@ export default {
     fontList() {
       return this.$store.state.customConfig.customFonts;
     },
+    selectedFont() {
+      return this.selectedFontFamilyIdx !== null ? this.fontList[this.selectedFontFamilyIdx] : 'sans-serif';
+    },
+    selectedFontStyle() {
+      return {
+        fontFamily: `${this.selectedFont}, 'sans-serif'`,
+      };
+    },
   },
 
   methods: {
     addFontFamily() {
-      this.$store.dispatch('addFontFamily', { name: this.fontInput });
+      const re = /[\w-_\s]+/g;
+      this.$store.dispatch('addFontFamily', { name: this.fontInput.match(re).join('') });
       this.fontInput = '';
     },
+    removeFontFamily({ idx }) {
+      this.$store.dispatch('removeFontFamily', { idx: parseInt(idx, 10) });
+      this.selectedFontFamilyIdx = null;
+    },
+    selectFont({ idx }) {
+      this.selectedFontFamilyIdx = idx;
+    },
+    checkForm(e) {
+      e.preventDefault();
+      if (this.fontInput) {
+        this.addFontFamily();
+        return true;
+      }
+      return false;
+    }
   },
 };
 </script>
 
 <style scoped lang="postcss">
-.Config-Page {
+.Config-page {
   padding: 1rem;
 }
 
-.Config-Heading {
+.Config-heading {
   font-size: 2rem;
+  margin-bottom: .5rem;
 }
 
-.Config-FontInput {
+.Config-fontInput {
   width: 100%;
   display: block;
   font-size: 1em;
@@ -90,25 +141,34 @@ export default {
   height: 2.25em;
   padding: 0em .5em;
   appearance: none;
-  max-width: 20em;
+  max-width: 21.25em;
   &:focus {
     text-align: start;
     color: black;
   }
 }
 
-.Config-InputArea {
+.Config-inputArea {
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
-  margin-top: 1rem;
+  margin: 1rem 1rem 1rem 0;
+  flex: 2 0 auto;
 }
 
-.Config-FontName {
-  font-family: sans-serif;
+.Config-mainArea {
+  display: flex;
+  flex-flow: row wrap;
+  margin-bottom: .5rem;
 }
 
-.Config-FontItem {
-  padding: .5em 0em;
+.Config-testArea {
+}
+
+.Config-fontItem {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: .25em 0em;
 }
 </style>
