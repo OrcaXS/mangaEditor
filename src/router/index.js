59cc842b05bac7b1dc23946675eb37b75035613f
OrcaXS Suen <sun16281394@gmail.com>
2018-07-14 00:04:16 +0800

    Remove Upload View

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Upload from '@/views/Upload';
 import Editor from '@/views/Editor';
 import Config from '@/views/Config';
 import Home from '@/views/Home';
@@ -10,11 +9,6 @@ Vue.use(Router);
 export default new Router({
   // mode: 'history',
   routes: [
-    {
-      path: '/upload',
-      name: 'upload',
-      component: Upload,
-    },
     {
       path: '/',
       name: 'home',
